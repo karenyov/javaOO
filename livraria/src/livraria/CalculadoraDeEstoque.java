4548503bada4f98c5c43f4828180ef4e6ce694c2
Karen Vicente <karenyasmin37@gmail.com>
2018-03-29 21:14:34 -0300

    add Capt 2 - Variáveis e tipos primitivos

---
+++ livraria/src/livraria/CalculadoraDeEstoque.java
@@ -0,0 +1,27 @@
+package livraria;
+
+/**
+ * @author Karen 29 de mar de 2018
+ */
+public class CalculadoraDeEstoque {
+
+	public static void main(String[] args) {
+
+		double soma = 0;
+
+		for (double i = 0; i < 35; i++) {
+			soma += 59.90;
+
+		}
+
+		System.out.println("O total em estoque é " + soma);
+
+		if (soma < 150) {
+			System.out.println("Seu estoque está muito baixo!");
+		} else if (soma >= 200) {
+			System.out.println("Seu estoque está muito alto!");
+		} else {
+			System.out.println("Seu estoque está bom");
+		}
+	}
+}
